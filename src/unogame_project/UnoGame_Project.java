6e04316aa97437b1b7197628d1e20b19964d7588
amrinder <amrinder@10.48.9.202>
2024-03-04 15:10:41 -0500

    Project Commmit 1

---
+++ src/unogame_project/UnoGame_Project.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package unogame_project;
+
+/**
+ *
+ * @author amrinder Singh
+ * Date:04/03/2024
+ * editor:Gurkirat Singh
+ */
+public class UnoGame_Project {
+
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
